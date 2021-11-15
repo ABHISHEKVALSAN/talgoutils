a5c1cf6bf6051377ec4df1d6ebace8706e8c0e21
Abhishek Valsan <abhishekvalsan9999@gmail.com>
2021-11-15 23:18:17 +0530

    initial commits

---
+++ data_reader/reader.py
@@ -0,0 +1,14 @@
+def read_csv(path):
+    return df
+
+def read_parquet(path):
+    return df
+
+def read_orc(path):
+    return df
+
+def read_gz(path):
+    return df
+
+def read_zip(path):
+    return df
