28546f97f1d2c3747efc85c534578e4492b0a39c
Abhishek Valsan <abhishekvalsan9999@gmail.com>
2021-11-27 23:22:40 +0530

    pandas lib

--- data_reader/reader.py
+++ data_reader/reader.py
@@ -1,13 +1,19 @@
+import pandas as pd
+
 def read_csv(path):
+    df = pd.read_csv(path)
     return df
 
 def read_parquet(path):
+    df = pd.read_parquet(path)
     return df
 
 def read_orc(path):
+    df = pd.read_orc(path)
     return df
 
 def read_gz(path):
+
     return df
 
 def read_zip(path):
