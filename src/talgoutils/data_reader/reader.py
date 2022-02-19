a396ff2a8cf6d441af433d052363f72624d008f5
Abhishek Valsan <abhishekvalsan9999@gmail.com>
2022-02-19 23:25:51 +0530

    adding read data in reader.py

--- src/talgoutils/data_reader/reader.py
+++ src/talgoutils/data_reader/reader.py
@@ -1,4 +1,15 @@
 import pandas as pd
+from talfoutils import constants as const
+
+def read_data(filename):
+    if filename.endswith(const.CSV):
+        return read_csv(filename)
+    elif filename.endsiwth(const.PARQUET):
+        return read_parquet(filename)
+    elif filename.endswith(const.ORC):
+        return read_orc(filename)
+    else:
+        print('Warning: Unknown file format for file {}'.format(filename))
 
 def read_csv(path):
     df = pd.read_csv(path)
@@ -13,7 +24,6 @@ def read_orc(path):
     return df
 
 def read_gz(path):
-
     return df
 
 def read_zip(path):
